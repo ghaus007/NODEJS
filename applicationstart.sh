462188d2efe9a1c0745c8e99ea5f51138fee8b6c
ghaus007 <93521154+ghaus007@users.noreply.github.com>
2022-03-14 18:03:22 +0530

    Update applicationstart.sh

--- applicationstart.sh
+++ applicationstart.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 cd /home/ubuntu
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
 npm i -g pm2
 pm2 -f start server.js
